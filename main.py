49b632bb8e0d58a575b76b354100f4964e4b735e
= <=>
2023-10-12 21:53:19 -0600

    correcciones en permisos de empleado

--- main.py
+++ main.py
@@ -3,115 +3,95 @@ import openpyxl
 
 # Función para el inicio de sesión
 def login():
-    print("Por favor, inicia sesión:")
-    username = input("Nombre de usuario: ")
-    password = getpass.getpass("Contraseña: ")
+    while True:
+        print("Por favor, inicia sesión:")
+        username = input("Nombre de usuario: ")
+        password = getpass.getpass("Contraseña: ")
 
-    # Verifica si es el administrador o un empleado
-    if username == "admin" and password == "adminpass":
-        return "admin"
-    elif username == "empleado" and password == "empleadopass":
-        return "empleado"
-    else:
-        print("Credenciales incorrectas. Inténtalo de nuevo.")
-        return None
-
-# Función para cargar productos desde un archivo Excel
-def cargar_productos():
-    productos = []
-    try:
-        workbook = openpyxl.load_workbook("productos.xlsx")
-        sheet = workbook.active
-        for row in sheet.iter_rows(min_row=2, values_only=True):
-            nombre, precio, cantidad = row
-            producto = {"nombre": nombre, "precio": precio, "cantidad": cantidad}
-            productos.append(producto)
-        print("Productos cargados exitosamente desde el archivo Excel.")
-    except FileNotFoundError:
-        print("El archivo de productos no existe. Debes agregar productos primero.")
-
-    return productos
-
-# Función para guardar los productos en un archivo Excel
-def guardar_productos(productos):
-    workbook = openpyxl.Workbook()
-    sheet = workbook.active
-
-    # Encabezados
-    sheet.append(["Nombre", "Precio de Venta", "Cantidad en Inventario"])
-
-    # Agrega los productos al archivo Excel
-    for producto in productos:
-        sheet.append([producto["nombre"], producto["precio"], producto["cantidad"]])
-
-    workbook.save("productos.xlsx")
+        if username == "admin" and password == "adminpass":
+            return "admin"
+        elif username == "empleado" and password == "empleadopass":
+            return "empleado"
+        else:
+            print("Credenciales incorrectas. Inténtalo de nuevo.")
 
 # Función para agregar un producto
-def agregar_producto(productos):
-    nombre = input("Nombre del producto: ")
-    precio = float(input("Precio de venta: "))
-    cantidad = int(input("Cantidad en inventario: "))
+def agregar_producto(productos, tipo_usuario):
+    if tipo_usuario == "admin":
+        nombre = input("Nombre del producto: ")
+        precio = float(input("Precio de venta: "))
+        cantidad = int(input("Cantidad en inventario: "))
 
-    producto = {"nombre": nombre, "precio": precio, "cantidad": cantidad}
-    productos.append(producto)
+        producto = {"nombre": nombre, "precio": precio, "cantidad": cantidad}
+        productos.append(producto)
 
-    print(f"{nombre} ha sido agregado al inventario.")
-
-def eliminar_producto(productos):
-    if not productos:
-        print("No hay productos para eliminar.")
-        return
-
-    print("Productos disponibles:")
-    for i, producto in enumerate(productos):
-        print(f"{i + 1}. {producto['nombre']}")
-
-    choice = int(input("Selecciona el número del producto que deseas eliminar: ")) - 1
-
-    if 0 <= choice < len(productos):
-        producto_eliminado = productos.pop(choice)
-        print(f"{producto_eliminado['nombre']} ha sido eliminado del inventario.")
-        guardar_productos(productos)  # Actualizar el archivo Excel
+        print(f"{nombre} ha sido agregado al inventario.")
     else:
-        print("Selección inválida.")
+        print("Acceso denegado. Debes ser admin para agregar productos.")
 
-def modificar_producto(productos):
-    if not productos:
-        print("No hay productos para modificar.")
-        return
-
-    print("Productos disponibles:")
-    for i, producto in enumerate(productos):
-        print(f"{i + 1}. {producto['nombre']}")
-
-    choice = int(input("Selecciona el número del producto que deseas modificar: ")) - 1
-
-    if 0 <= choice < len(productos):
-        producto = productos[choice]
-        print("Datos actuales del producto:")
-        print(f"Nombre: {producto['nombre']}")
-        print(f"Precio de venta: {producto['precio']}")
-        print(f"Cantidad en inventario: {producto['cantidad']}")
-
-        nombre = input("Nuevo nombre (dejar en blanco para no cambiar): ")
-        if nombre:
-            producto['nombre'] = nombre
-
-        precio = input("Nuevo precio de venta (dejar en blanco para no cambiar): ")
-        if precio:
-            producto['precio'] = float(precio)
-
-        cantidad = input("Nueva cantidad en inventario (dejar en blanco para no cambiar): ")
-        if cantidad:
-            producto['cantidad'] = int(cantidad)
-
-        print(f"{producto['nombre']} ha sido modificado en el inventario.")
-        guardar_productos(productos)  # Actualizar el archivo Excel
+# Función para eliminar un producto
+def eliminar_producto(productos, tipo_usuario):
+    if tipo_usuario == "admin":
+        if not productos:
+            print("No hay productos para eliminar.")
+            return
+        
+        print("Productos disponibles:")
+        for i, producto in enumerate(productos):
+            print(f"{i + 1}. {producto['nombre']}")
+        
+        choice = int(input("Selecciona el número del producto que deseas eliminar: ")) - 1
+        
+        if 0 <= choice < len(productos):
+            producto_eliminado = productos.pop(choice)
+            print(f"{producto_eliminado['nombre']} ha sido eliminado del inventario.")
+            guardar_productos(productos)  # Actualizar el archivo Excel
+        else:
+            print("Selección inválida.")
     else:
-        print("Selección inválida.")
+        print("Acceso denegado. Debes ser admin para eliminar productos.")
+
+# Función para modificar un producto
+def modificar_producto(productos, tipo_usuario):
+    if tipo_usuario == "admin":
+        if not productos:
+            print("No hay productos para modificar.")
+            return
+        
+        print("Productos disponibles:")
+        for i, producto in enumerate(productos):
+            print(f"{i + 1}. {producto['nombre']}")
+        
+        choice = int(input("Selecciona el número del producto que deseas modificar: ")) - 1
+        
+        if 0 <= choice < len(productos):
+            producto = productos[choice]
+            print("Datos actuales del producto:")
+            print(f"Nombre: {producto['nombre']}")
+            print(f"Precio de venta: {producto['precio']}")
+            print(f"Cantidad en inventario: {producto['cantidad']}")
+            
+            nombre = input("Nuevo nombre (dejar en blanco para no cambiar): ")
+            if nombre:
+                producto['nombre'] = nombre
+            
+            precio = input("Nuevo precio de venta (dejar en blanco para no cambiar): ")
+            if precio:
+                producto['precio'] = float(precio)
+            
+            cantidad = input("Nueva cantidad en inventario (dejar en blanco para no cambiar): ")
+            if cantidad:
+                producto['cantidad'] = int(cantidad)
+            
+            print(f"{producto['nombre']} ha sido modificado en el inventario.")
+            guardar_productos(productos)  # Actualizar el archivo Excel
+        else:
+            print("Selección inválida.")
+    else:
+        print("Acceso denegado. Debes ser admin para modificar productos.")
 
 # Función para la sección de caja de cobro
-def caja_de_cobro(productos):
+def caja_de_cobro(productos, tipo_usuario):
     caja = []
     total = 0
 
@@ -120,7 +100,8 @@ def caja_de_cobro(productos):
         print("1. Agregar producto a la caja")
         print("2. Eliminar producto de la caja")
         print("3. Hacer corte de caja")
-        print("4. Cobrar productos y salir")
+        print("4. Cobrar productos y volver a la caja")
+        print("5. Salir de la caja")
 
         opcion = input("Selecciona una opción: ")
 
@@ -167,22 +148,56 @@ def caja_de_cobro(productos):
                         print("El monto en efectivo es insuficiente.")
                     else:
                         print(f"¡Cambio: ${cambio}")
+                        caja = []  # Limpiar la caja después del cobro
+                        total = 0
                 elif metodo_pago == "tarjeta":
                     print("Por favor, verifique que el cobro se haya efectuado de manera correcta.")
-                    caja = []
+                    caja = []  # Limpiar la caja después del cobro
                     total = 0
                 else:
                     print("Método de pago inválido.")
                     continue
+        elif opcion == "5":
             break
         else:
             print("Opción inválida.")
 
+
+# Función para guardar los productos en un archivo Excel
+def guardar_productos(productos):
+    workbook = openpyxl.Workbook()
+    sheet = workbook.active
+
+    # Encabezados
+    sheet.append(["Nombre", "Precio de Venta", "Cantidad en Inventario"])
+
+    # Agrega los productos al archivo Excel
+    for producto in productos:
+        sheet.append([producto["nombre"], producto["precio"], producto["cantidad"]])
+
+    workbook.save("productos.xlsx")
+
+# Función para cargar productos desde un archivo Excel
+def cargar_productos():
+    try:
+        workbook = openpyxl.load_workbook("productos.xlsx")
+        sheet = workbook.active
+        productos = []
+
+        for row in sheet.iter_rows(min_row=2, values_only=True):
+            nombre, precio, cantidad = row
+            producto = {"nombre": nombre, "precio": precio, "cantidad": cantidad}
+            productos.append(producto)
+
+        return productos
+    except FileNotFoundError:
+        return []
+
 if __name__ == "__main__":
     tipo_usuario = login()
-    if tipo_usuario == "admin":
-        productos = cargar_productos()
+    productos = cargar_productos()
 
+    if tipo_usuario == "admin":
         while True:
             print("\nOpciones:")
             print("1. Agregar producto")
@@ -194,19 +209,31 @@ if __name__ == "__main__":
             opcion = input("Selecciona una opción: ")
 
             if opcion == "1":
-                agregar_producto(productos)
+                agregar_producto(productos, tipo_usuario)
             elif opcion == "2":
-                eliminar_producto(productos)
+                eliminar_producto(productos, tipo_usuario)
             elif opcion == "3":
-                modificar_producto(productos)
+                modificar_producto(productos, tipo_usuario)
             elif opcion == "4":
-                caja_de_cobro(productos)
+                caja_de_cobro(productos, tipo_usuario)
             elif opcion == "5":
                 guardar_productos(productos)
                 break
             else:
                 print("Opción inválida.")
     elif tipo_usuario == "empleado":
-        print("Bienvenido, empleado. Acceso limitado.")
+        while True:
+            print("\nOpciones:")
+            print("1. Caja de cobro")
+            print("2. Salir")
+
+            opcion = input("Selecciona una opción: ")
+
+            if opcion == "1":
+                caja_de_cobro(productos, tipo_usuario)
+            elif opcion == "2":
+                break
+            else:
+                print("Opción inválida.")
     else:
         print("Acceso denegado. Hasta luego.")
